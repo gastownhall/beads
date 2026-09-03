19c5c699e350bbbcd358321b375a2ac4baf52cd2
Julian Knutsen <julianknutsen@users.noreply.github.com>
2026-09-03 00:49:47 +0000

    test(proxy): provide valid direct-only command args

--- cmd/bd/proxied_direct_only_refusal_frontdoor_test.go
+++ cmd/bd/proxied_direct_only_refusal_frontdoor_test.go
@@ -12,7 +12,7 @@ func TestProxiedDirectOnlyRefusalsFrontDoor(t *testing.T) {
 	requireManagedLocalProxiedEnv(t)
 	bd := buildEmbeddedBD(t)
 	p := bdManagedLocalInit(t, bd, "directonly", 5*time.Minute)
-	for _, args := range [][]string{{"sync"}, {"dolt", "remote", "add"}, {"dolt", "remote", "list"}, {"dolt", "remote", "reset-data"}, {"dolt", "push"}, {"dolt", "pull"}, {"dolt", "commit"}} {
+	for _, args := range [][]string{{"sync"}, {"dolt", "remote", "add", "origin", "https://example.com/repo"}, {"dolt", "remote", "list"}, {"dolt", "remote", "reset-data", "origin"}, {"dolt", "push"}, {"dolt", "pull"}, {"dolt", "commit"}} {
 		t.Run(strings.Join(args, "/"), func(t *testing.T) {
 			out, errOut, err := bdProxiedRunBuffers(t, bd, p.dir, append(args, "--json")...)
 			if err == nil {
